9b7403cd22cd57aac91ab4dd33f5a49fd36281d3
Remus09 <remusracolta09@gmail.com>
2017-01-10 20:25:55 +0200

    Initial commit

---
+++ src/package1/loan_calculator.java
@@ -0,0 +1,32 @@
+package package1;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Remus on 1/8/2017.
+ */
+public class loan_calculator {
+    private JPanel rootPaenl;
+    private JButton Click_me;
+
+    public loan_calculator() {
+
+
+        Click_me.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null;"Hello world!");
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame
+    }
+
+
+    }
+
